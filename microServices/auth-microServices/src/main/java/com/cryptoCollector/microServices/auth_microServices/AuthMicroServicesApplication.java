33b66ead8882386b015358e5dc038cf1dd888906
AlejoAlzate3 <alejoalzate1@gmail.com>
2025-10-21 00:24:04 -0500

    Estrutura inicial de microservicios de autenticación

---
+++ microServices/auth-microServices/src/main/java/com/cryptoCollector/microServices/auth_microServices/AuthMicroServicesApplication.java
@@ -0,0 +1,13 @@
+package com.cryptoCollector.microServices.auth_microServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthMicroServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthMicroServicesApplication.class, args);
+	}
+
+}
---
+++ microServices/auth-microServices/src/test/java/com/cryptoCollector/microServices/auth_microServices/AuthMicroServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptoCollector.microServices.auth_microServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthMicroServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
